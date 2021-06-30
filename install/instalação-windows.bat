20668e7b6f446706c2bc8b2784301d97b62ba3a2
vpdiogo <vitorpdiogo@gmail.com>
2021-06-30 11:28:38 -0300

    atualização em scripts de instalação e readme

---
+++ install/instalação-windows.bat
@@ -0,0 +1,10 @@
+cmd /k "cd .. & py -m venv env & .\env\Scripts\activate & pip install openpyxl"
+
+echo.
+echo    ***********************************************
+echo    ***                                         ***
+echo    ***          INSTALAÇÃO FINALIZADA!         ***
+echo    ***                                         ***
+echo    ***********************************************
+echo.
+@pause
